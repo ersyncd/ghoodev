83c287923d6fd2524edfc15ecc993eb7cd59f7e7
Ersync <teguhersyarudin@gmail.com>
2026-07-02 21:19:18 +0700

    feat: add @sveltejs/enhanced-img for improved image handling and performance

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { enhancedImages } from '@sveltejs/enhanced-img';
 
 export default defineConfig({
 	optimizeDeps: {
 		include: ['clsx', 'devalue']
 	},
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [enhancedImages(), tailwindcss(), sveltekit()]
 });
